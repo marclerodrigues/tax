6992b1980ddc60dccafb6257647544fb51c8520c
Marcle Rodrigues <marcle@getwaysync.com>
2023-05-16 00:53:40 -0300

    Initial commit

---
+++ lib/tax.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tax/version"
+
+module Tax
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tax/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tax
+  VERSION = "0.1.0"
+end
